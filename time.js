e58f8aa14e0695f6dadb5bbcafcf95d39c3084cc
Flashxu98 <dinhxuan9807@gmail.com>
2018-11-18 12:47:46 +0700

    push lan 2

--- time.js
+++ time.js
@@ -1,37 +1,31 @@
 
+
 document.addEventListener('DOMContentLoaded', function(){
-
+    var myVar = setInterval(myTimer, 1);
     var countDownDate = new Date("Jun 28, 2016 00:00:00").getTime();
-
-
     // Update the count down every 1 second
     var x = setInterval(function() {
-    
+
         // Get todays date and time
         var now = new Date().getTime();
         
         // Find the distance between now and the count down date
-        var distance =now - countDownDate;
+        var distance = now - countDownDate;
         
         // Time calculations for days, hours, minutes and seconds
         var days = Math.floor(distance / (1000 * 60 * 60 * 24));
         var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
         var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-        
         // Output the result in an element with id="demo"
-        document.querySelector(".date span").innerHTML = days + " days / " + hours + "hours  /"
-        + minutes + "m  " + seconds + "s ";
-        
+        document.querySelector(".date span").innerHTML = days + "days&nbsp-&nbsp" + hours + "h&nbsp-&nbsp "
+        + minutes + "m&nbsp-&nbsp" + seconds +"s&nbsp" ;
         // If the count down is over, write some text 
         if (distance < 0) {
             clearInterval(x);
             document.querySelector(".date span").innerHTML = "EXPIRED";
         }
-    }, 1000);
-
-
-    var myVar = setInterval(myTimer, 1000);
+    }, 1);
 
     function myTimer() {
         var d = new Date();
